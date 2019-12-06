a48653fa051c8a58b70df40717a1ed01294b6d89
Proudust <proudust@gmail.com>
2019-12-06 11:54:38 +0900

    :rotating_light: Removing ESLint warnings

--- src/wait.ts
+++ src/wait.ts
@@ -1,9 +1,9 @@
-export function wait(milliseconds: number) {
-    return new Promise((resolve) => {
-        if (isNaN(milliseconds)) {
-            throw new Error('milliseconds not a number');
-        }
+export function wait(milliseconds: number): Promise<string> {
+  return new Promise<string>(resolve => {
+    if (isNaN(milliseconds)) {
+      throw new Error('milliseconds not a number');
+    }
 
-        setTimeout(() => resolve("done!"), milliseconds)
-    });
+    setTimeout(() => resolve('done!'), milliseconds);
+  });
 }
